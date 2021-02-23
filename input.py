379e45274b50e3fe9b9187d92eb181dc22e22422
Arunsp2000 <56639917+Arunsp2000@users.noreply.github.com>
2021-02-23 21:56:56 +0530

    update-1

---
+++ input.py
@@ -0,0 +1,27 @@
+import cv2
+import os
+import sys
+
+
+inp=cv2.VideoCapture("text.mp4")
+inp.set(1,100)
+
+try:
+    if not os.path.exists('inp_images'):
+        os.makedirs('inp_images')
+except OSError:
+    print('Error creating input directory')
+
+curr_frame=0
+while(1):
+    ret,frame=inp.read()
+    if(ret):
+        name = './inp_images/inp_image_' + str(curr_frame) + '.jpg'
+        print('Creating...'+ name)
+        cv2.imwrite(name,frame)
+        curr_frame+=1
+    else:
+        break
+
+inp.release()
+cv2.destroyAllWindows()
