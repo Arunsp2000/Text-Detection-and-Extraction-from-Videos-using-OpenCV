681d5a5c25311df0dbe7209740aa0a89d3cb1b9f
Arunsp2000 <arunsrinipartha@gmail.com>
2021-03-01 12:58:11 +0530

    East_Detection_Model and code added

---
+++ east_detector.py
@@ -0,0 +1,119 @@
+from imutils.object_detection import non_max_suppression
+import numpy as np
+import argparse
+import time
+import cv2
+import os
+import pytesseract 
+
+def captch_ex(folder):
+    for filename in os.listdir(folder):
+        image = cv2.imread(os.path.join(folder, filename))
+        orig = image.copy()
+        extract_name='./text_extraction/text_extract_' + filename[:-4] + ".txt"
+        file = open(extract_name, "w+")
+        text = pytesseract.image_to_string(orig)  
+        file.write(text) 
+        # file.write("\n")   
+        file.close()
+        (H, W) = image.shape[:2]
+        # set the new width and height and then determine the ratio in change
+        # for both the width and height
+        (newW, newH) = (320, 320)
+        rW = W / float(newW)
+        rH = H / float(newH)
+        # resize the image and grab the new image dimensions
+        image = cv2.resize(image, (newW, newH))
+        (H, W) = image.shape[:2]
+        layerNames = ["feature_fusion/Conv_7/Sigmoid",
+                      "feature_fusion/concat_3"]
+
+        net = cv2.dnn.readNet("frozen_east_text_detection.pb")
+        # construct a blob from the image and then perform a forward pass of
+        # the model to obtain the two output layer sets
+        blob = cv2.dnn.blobFromImage(
+            image, 1.0, (W, H), (123.68, 116.78, 103.94), swapRB=True, crop=False)
+        net.setInput(blob)
+        (scores, geometry) = net.forward(layerNames)
+
+        # grab the number of rows and columns from the scores volume, then
+        # initialize our set of bounding box rectangles and corresponding
+        # confidence scores
+        (numRows, numCols) = scores.shape[2:4]
+        rects = []
+        confidences = []
+        # loop over the number of rows
+        for y in range(0, numRows):
+            # extract the scores (probabilities), followed by the geometrical
+            # data used to derive potential bounding box coordinates that
+            # surround text
+            scoresData = scores[0, 0, y]
+            xData0 = geometry[0, 0, y]
+            xData1 = geometry[0, 1, y]
+            xData2 = geometry[0, 2, y]
+            xData3 = geometry[0, 3, y]
+            anglesData = geometry[0, 4, y]
+
+            # loop over the number of columns
+            for x in range(0, numCols):
+                # if our score does not have sufficient probability, ignore it
+                if scoresData[x] < 0.5:
+                    continue
+                # compute the offset factor as our resulting feature maps will
+                # be 4x smaller than the input image
+                (offsetX, offsetY) = (x * 4.0, y * 4.0)
+                # extract the rotation angle for the prediction and then
+                # compute the sin and cosine
+                angle = anglesData[x]
+                cos = np.cos(angle)
+                sin = np.sin(angle)
+                # use the geometry volume to derive the width and height of
+                # the bounding box
+                h = xData0[x] + xData2[x]
+                w = xData1[x] + xData3[x]
+                # compute both the starting and ending (x, y)-coordinates for
+                # the text prediction bounding box
+                endX = int(offsetX + (cos * xData1[x]) + (sin * xData2[x]))
+                endY = int(offsetY - (sin * xData1[x]) + (cos * xData2[x]))
+                startX = int(endX - w)
+                startY = int(endY - h)
+                # add the bounding box coordinates and probability score to
+                # our respective lists
+                rects.append((startX, startY, endX, endY))
+                confidences.append(scoresData[x])
+
+        # apply non-maxima suppression to suppress weak, overlapping bounding
+        # boxes
+        boxes = non_max_suppression(np.array(rects), probs=confidences)
+        # loop over the bounding boxes
+        for (startX, startY, endX, endY) in boxes:
+            # scale the bounding box coordinates based on the respective
+            # ratios
+            startX = int(startX * rW)
+            startY = int(startY * rH)
+            endX = int(endX * rW)
+            endY = int(endY * rH)
+            # draw the bounding box on the image
+            cv2.rectangle(orig, (startX, startY), (endX, endY), (0, 255, 0), 2)
+        # show the output image
+        # cv2.imshow("Text Detection", orig)
+        # cv2.waitKey(0)
+        name = './text_detection/text_detect_' + filename
+        cv2.imwrite(name, orig)
+
+
+try:
+    if not os.path.exists('text_detection'):
+        os.makedirs('text_detection')
+except OSError:
+    print('Error creating input directory')
+
+try:
+    if not os.path.exists('text_extraction'):
+        os.makedirs('text_extraction')
+except OSError:
+    print('Error creating input directory')
+
+
+folder = "./inp_images"
+captch_ex(folder)
